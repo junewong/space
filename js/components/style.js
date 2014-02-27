c33b34d2b2c3b819094c8a63a08d81748baeb338
June Wong <junewong12@gmail.com>
2014-02-27 11:31:24 +0800

    add Disqus comment

--- js/components/style.js
+++ js/components/style.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
 
 	$('h1').addClass('text-primary').addClass("page-header");
 	$('h2').addClass("page-header");
-	$('#menu > ul').addClass('nav nav-pills nav-stacked').addClass('list-group');
+	$('#menu').find('ul').addClass('list-group');
 	$('#menu > ul > li').addClass('list-group-item');
-	//$('#menu').scrollspy();
-
+	$('#contents').scrollspy({target: '#menu'});
 });
--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 //include('libs/jquery.min.js');
 include("components/url.js");
 include('components/sliderbar.js');
-include('components/navigation.js');
+//include('components/navigation.js');
 include('components/content.js');
 //include('components/folder.js');
 include('components/style.js');
