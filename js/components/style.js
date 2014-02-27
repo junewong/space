af178e3ae4184bca6f889f2d885f36e85ead6a2f
June Wong <junewong12@gmail.com>
2014-02-27 16:52:18 +0800

    modify some detail.

--- js/components/style.js
+++ js/components/style.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 	$('h1').addClass('text-primary').addClass("page-header");
 	$('h2').addClass("page-header");
-	$('#menu').find('ul').addClass('list-group');
+	$('#menu > ul').addClass('list-group');
 	$('#menu > ul > li').addClass('list-group-item');
 	$('#contents').scrollspy({target: '#menu'});
 });
