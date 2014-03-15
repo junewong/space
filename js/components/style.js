7ef90fbd856c20cb573556bd6c1e28523cb77f8e
June Wong <junewong12@gmail.com>
2014-03-15 16:52:29 +0800

    add some images.

--- js/components/style.js
+++ js/components/style.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	$('#contents h1').addClass('text-primary').addClass("page-header");
 	$('#contents h2').addClass("page-header");
 	$('#contents img').addClass("img-responsive");
+	$('#contents em').addClass("text-muted");
 
 	$('#menu').bind('DOMNodeInserted', function(e) {
 		$('#menu > ul').addClass('list-group');
