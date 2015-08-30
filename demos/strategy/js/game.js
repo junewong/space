8cb1329b4e45b817fea354a0b3a29c923253d74a
June Wong <junewong12@gmail.com>
2015-08-30 19:18:35 +0800

    add

---
+++ demos/strategy/js/game.js
@@ -0,0 +1,143 @@
+
+var StrategyGame = Class({
+
+	init : function() {
+		this.strategies = [];
+		this.step = 0;
+		this.maxStep = 100;
+		this.value = 0;
+		this.total = 0;
+
+		this._isPlaying = false;
+		this.handle = null;
+
+		this.dealCallback = null;
+		this.deathCallback = null;
+		this.nextCallback = null;
+		this.endCallback = null;
+	},
+
+	setMaxStep : function( maxStep ) {
+		this.maxStep = maxStep;
+	},
+
+	next : function() {
+		this.step += 1;
+
+		for ( var i in this.strategies ) {
+			var a = this.strategies[i];
+			if ( a.isDead() ) {
+				continue;
+			}
+
+			for ( var j in this.strategies ) {
+				var b = this.strategies[j];
+				if ( a === b || a.isDead() || b.isDead() ) {
+					continue;
+				}
+				this._deal( a, b );
+			}
+		}
+
+		if ( this.nextCallback ) {
+			this.nextCallback( this.step );
+		}
+
+		if ( this.step >= this.maxStep ) {
+			this.pause();
+			if ( this.endCallback ) {
+				this.endCallback( this.step );
+			}
+		}
+	},
+
+	addStrategy : function( strategy ) {
+		this.strategies.push( strategy );
+		strategy.setId ( this.strategies.length );
+
+		this.total += strategy.getValue();
+	},
+
+	getTotal : function() {
+		return this.total;
+	},
+
+	play : function() {
+		var _this = this;
+
+		this._isPlaying = true;
+
+		function autoRun() {
+			// 获取每次的动画帧
+			setTimeout( function() {
+				if ( _this._isPlaying ) {
+					_this.handle = requestAnimationFrame( autoRun );
+					_this.next();
+				}
+			}, 150 );
+		}
+
+		autoRun();
+	},
+
+	pause : function() {
+		this._isPlaying = false;
+		cancelAnimationFrame( this.handle );
+		this.handle  = null;
+	},
+
+	isPlaying : function() {
+		return !! this._isPlaying;
+	},
+
+	_changeTotal : function( offset ) {
+		this.total += offset;
+	},
+
+	_deal : function( a, b ) {
+		var action = a.deal( b );
+		a.increaseStep();
+		a.context.addAction( b.getId(), action );
+
+		// 没有动作
+		if ( action === ACTION_NONE ) {
+			return;
+		}
+
+		var answer = b.answer( a, action );
+		var valueA = 0, valueB = 0, total = 0;
+
+		if ( answer == ACTION_COOPERATE ) {
+			valueA = 1;
+			valueB = 1;
+			total = 2;
+
+		} else if ( answer == ACTION_CHEAT) {
+			valueA = -2;
+			valueB = 2;
+			total = 0;
+		}
+
+		a.changeValue( valueA );
+		b.changeValue( valueB );
+		this._changeTotal( total );
+
+		a.context.addOtherAction( b.getId(), answer );
+
+		if ( this.dealCallback ) {
+			this.dealCallback( a, valueA, b, valueB, total );
+		}
+
+		if ( this.deathCallback ) {
+			if ( a.isDead() ) {
+				this.deathCallback( a );
+			}
+			if ( b.isDead() ) {
+				this.deathCallback( b );
+			}
+		}
+
+	}
+
+});
+
---
+++ demos/strategy/js/strategy.js
@@ -0,0 +1,214 @@
+var ALL_STRAGEGY = [];
+
+var Context = Class({
+	init : function() {
+		this.value = 10;
+		this.actions = {};
+		this.otherActions = {};
+	},
+
+	getValue : function() {
+		return this.value;
+	},
+
+	appendValue : function( offset ) {
+		this.value += offset;
+	},
+
+	getLastAction : function() {
+		var history = this.actions[ id ];
+		var length = history && history.length || 0;
+		return length > 0 ? history[ length - 1 ] : null;
+	},
+
+	addAction: function( id, action ) {
+		var otherPerson = this.actions[ id ];
+		if ( ! otherPerson ) {
+			otherPerson = [];
+			this.actions[ id ] = otherPerson;
+		}
+		otherPerson.push( action ); 
+	},
+
+	addOtherAction : function( id, action ) {
+		var otherPerson = this.otherActions[ id ];
+		if ( ! otherPerson ) {
+			otherPerson = [];
+			this.otherActions[ id ] = otherPerson;
+		}
+		otherPerson.push( action ); 
+	}, 
+
+	getOtherLastAction : function( id ) {
+		var history = this.otherActions[ id ];
+		var length = history && history.length || 0;
+		return length > 0 ? history[ length - 1 ] : null;
+	}
+});
+
+/**
+ * 策略的基类
+ */
+var StrategyBase = Class({
+	init : function( id ) {
+		this._init( id );
+	},
+
+	_init : function( id, name ) {
+		this.context =  new Context();
+		this.id = id;
+		this.name = name;
+		this.description = '';
+		this.step = 0;
+
+		this.changeValueCallback = null;
+		this.deathCallback = null;
+	},
+
+	setId : function( id ) {
+		this.id = id;
+	},
+
+	getId : function() {
+		return this.id;
+	},
+
+	getName : function() {
+		return this.name;
+	},
+
+	getValue : function() {
+		return this.context.getValue();
+	},
+
+	changeValue : function( offset ) {
+		this.context.appendValue( offset );
+
+		if ( this.changeValueCallback ) {
+			this.changeValueCallback( offset );
+		}
+
+		if ( this.getValue() <= 0 ) {
+			if ( this.deathCallback ) {
+				this.deathCallback( this.getValue() );
+			}
+			return;
+		}
+	},
+
+	deal : function( other ) {
+	},
+
+	answer : function( other, action ) {
+	},
+
+	isDead : function() {
+		return this.getValue() <= 0;
+	},
+
+	increaseStep : function() {
+		this.step += 1;
+	}
+});
+
+/*
+ * 以牙还牙策略
+ */
+var TitForTatStrategy = Class( StrategyBase, {
+
+	init : function( id ) {
+		this._init( id, '以牙还牙' );
+		this.description = '根据跟对方上一个回合的交易情况，如果上次是欺诈，则报复，否则则合作。';
+	},
+
+	// 如果对方上一次是合作，则合作，否则报复；默认是合作；
+	deal : function( other ) {
+		var lastOtherAction = this.context.getOtherLastAction( other.id );
+		if ( lastOtherAction === ACTION_CHEAT) {
+			return ACTION_NONE;
+		}
+		return ACTION_COOPERATE;
+	},
+
+	// 根据对方上一次做相同反应，否则选择合作
+	answer : function( other ) {
+		var lastOtherAction = this.context.getOtherLastAction( other.id );
+		if ( lastOtherAction === null) {
+			return ACTION_COOPERATE;
+		}
+		return lastOtherAction;
+	}
+
+});
+ALL_STRAGEGY.push( TitForTatStrategy );
+
+
+/**
+ * 每次都是找人合作，但是遇到别人找他合作的时候就使用欺诈。
+ */
+var CheatStrategy = Class( StrategyBase, {
+
+	init : function( id ) {
+		this._init( id, '欺诈师' );
+		this.description = '主动时则总是选择发起合作，但如果别人找他合作，则使用欺诈。';
+	},
+
+	// 如果对方上一次是合作，则合作，否则报复；默认是合作；
+	deal : function( other ) {
+		return ACTION_COOPERATE;
+	},
+
+	// 根据对方上一次做相同反应，否则选择合作
+	answer : function( other ) {
+		return ACTION_CHEAT;
+	}
+
+});
+ALL_STRAGEGY.push( CheatStrategy );
+
+
+var CooperateStrategy = Class( StrategyBase, {
+
+	init : function( id ) {
+		this._init( id, '遵纪守法' );
+		this.description = '如果对方上次有欺诈过，则不主动合作；但任何发起合作的时候都答应跟对方合作';
+	},
+
+
+	// 如果对方上一次是合作，则合作，否则报复；默认是合作；
+	deal : function( other ) {
+		var lastOtherAction = this.context.getOtherLastAction( other.id );
+		if ( lastOtherAction === ACTION_CHEAT) {
+			return ACTION_NONE;
+		}
+		return ACTION_COOPERATE;
+	},
+
+	// 根据对方上一次做相同反应，否则选择合作
+	answer : function( other ) {
+		return ACTION_COOPERATE;
+	}
+
+});
+ALL_STRAGEGY.push( CooperateStrategy );
+
+
+var DestroyStrategy = Class( StrategyBase, {
+
+	init : function( id ) {
+		this._init( id, '毁灭人格' );
+		this.description = '从不主动跟对方合作，遇到有人发起合作，则选择欺诈。';
+	},
+
+	// 如果对方上一次是合作，则合作，否则报复；默认是合作；
+	deal : function( other ) {
+		return ACTION_COOPERATE;
+	},
+
+	// 根据对方上一次做相同反应，否则选择合作
+	answer : function( other ) {
+		return ACTION_CHEAT;
+	}
+
+});
+ALL_STRAGEGY.push( DestroyStrategy );
