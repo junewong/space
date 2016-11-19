704b98c7f8f3fe786dae84a09bc4b51b5ff86395
junewong <junewong12@gmail.com>
2016-11-20 02:21:14 +0800

    update weixin articles

--- wx/publish.sh
+++ wx/publish.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo "try to publish ..."
+echo "pulling ..."
 git pull ;
+echo "adding ..."
 git add . ;
+echo "commiting ..."
 git commit -a -m "update weixin articles" ;
+echo "pushing ..."
 git push ;
 echo "Done"
